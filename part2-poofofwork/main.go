f9742ddd6ea25f2a192f0236317ab62090a89f10
王彬彬 <769558579@qq.com>
2019-01-10 00:04:42 +0800

    1. proof of work  POW 工作量证明

---
+++ part2-poofofwork/main.go
@@ -0,0 +1,28 @@
+// auth: kunlun
+// date: 2019-01-09
+// description:
+package main
+
+import (
+	"blc2"
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	blockChain := blc2.NewBlockChain()
+	blockChain.AddBlock("send 10 btc to zhangsan")
+	blockChain.AddBlock("send 10 btc to lisi")
+	blockChain.AddBlock("send 10 btc to zhaoliu")
+
+	for _, block := range blockChain.Block {
+		fmt.Printf("Data: %s \n", string(block.Data))
+		fmt.Printf("PreBlockHash: %x \n", string(block.PrevBlockHash))
+		fmt.Printf("TimeStamp: %s \n", time.Unix(block.Timestamp, 0).Format("2006-01-02 03:04:05 PM"))
+		fmt.Printf("Hash: %x \n", block.Hash)
+		fmt.Printf("Nonce: %d \n", block.Nonce)
+
+	}
+
+}
